17a6bf01a19ed164d7619f767d3a3754c61e95a9
jingwei <jingwei@ldomotors.com>
2019-07-19 12:00:15 +0800

    Add easy web code

---
+++ Application/EasyWeb/include/easyweb.h
@@ -0,0 +1,27 @@
+/******************************************************************
+ *****                                                        *****
+ *****  Name: easyweb.h                                       *****
+ *****  Ver.: 1.0                                             *****
+ *****  Date: 07/05/2001                                      *****
+ *****  Auth: Andreas Dannenberg                              *****
+ *****        HTWK Leipzig                                    *****
+ *****        university of applied sciences                  *****
+ *****        Germany                                         *****
+ *****  Func: header-file for easyweb.c                       *****
+ *****                                                        *****
+ ******************************************************************/
+
+#ifndef __EASYWEB_H
+#define __EASYWEB_H
+#include "lpc_types.h"
+
+void InitOsc(void);                        // prototypes
+void InitPorts(void);
+void HTTPServer(void);
+void InsertDynamicValues(void);
+
+void Easy_Web_Init(void);
+void Easy_Web_Execute(void);
+
+#endif
+
